2268386ea3c7401db15d8a665d832096b41e04ef
Huynh Tuan Anh <20520383@gm.uit.edu.vn>
2022-11-30 01:16:56 +0700

    thuy vi

---
+++ main.js
@@ -0,0 +1,65 @@
+function createHeart() {
+  const hearts = document.createElement('div');
+  
+  hearts.classList.add('hearts');
+  
+  hearts.style.left = Math.random() * 100 + "vw";
+  hearts.style.animationDuration = Math.random() * 2 + 3 + "s";
+  
+  hearts.innerText = '❤️';
+  
+  document.body.appendChild(hearts);
+  
+  setTimeout(() => {
+    hearts.remove();
+  }, 5000);
+}
+
+
+
+
+var player = false;
+const heart = document.querySelector('.heart');
+const circle = document.querySelector('.circle');
+const container = document.querySelector('.container');
+const save = document.querySelector('.save');
+const music = document.querySelector('.music')
+function toggleHeart() {
+  heart.classList.toggle("heart-filled"); 
+  circle.classList.toggle("circle-show");
+  container.classList.toggle("container-shadow");
+  document.body.classList.toggle("background-heart");
+  heart.style.animationPlayState = "running";
+  setInterval(createHeart, 50)
+  showSaved();
+  if (player == false) 
+    play()
+  else
+    pause()
+}
+function play() {
+  music.play();
+  player = true
+}
+function pause() {
+  music.pause();
+  player = false
+}
+
+
+heart.addEventListener('click', toggleHeart);
+
+function showSaved() {
+  
+  if(heart.classList.contains("heart-filled")){
+      save.innerHTML = 'Thuy Vi';
+    
+  } else {
+     save.innerHTML = ''; 
+
+  }
+
+}
+
+
+
